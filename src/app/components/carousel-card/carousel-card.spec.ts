e8a696fa56a828feedb67a88943ffd30642aca43
Allana <allanamedradoo@gmail.com>
2026-09-24 17:42:04 -0300

    correção dos testes e criação de arquivo yml para ci

--- src/app/components/carousel-card/carousel-card.spec.ts
+++ src/app/components/carousel-card/carousel-card.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { describe, beforeEach, it, expect } from 'vitest';
 import { CarouselCardComponent } from './carousel-card';
 
 describe('CarouselCardComponent', () => {
--- src/app/layout/banner/banner.spec.ts
+++ src/app/layout/banner/banner.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { describe, beforeEach, it, expect } from 'vitest';
 import { Banner } from './banner';
 
 describe('Banner', () => {
